d23a325b69b96407c60edd50577cb348e1e96ca7
Ivan Sanchez <ivan@gourame.com>
2021-03-17 10:37:50 +0000

    Upload function package

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("http4k-storage-example");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+const functionPackage = new aws.s3.BucketObject("http4k-storage-lambda-package", {
+    key: "deployment/storage-examples-1.0-SNAPSHOT.zip",
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset("build/distributions/storage-examples-1.0-SNAPSHOT.zip")
+});
