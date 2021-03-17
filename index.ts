6ff9f4c51be07a606df933a929b4ed9e067a5453
Ivan Sanchez <ivan@gourame.com>
2021-03-17 15:27:02 +0000

    Log from cloudwatch

--- index.ts
+++ index.ts
@@ -20,11 +20,23 @@ const defaultRole = new aws.iam.Role("http4k-storage-example-lambda-default-role
 `
 });
 
+const logGroupApi = new aws.cloudwatch.LogGroup("http4k-storage-example-api-route", {
+    name: "http4k-storage-example",
+});
+
 const storageFunction = new aws.lambda.Function("http4k-storage-example-lambda", {
     code: new pulumi.asset.FileArchive("build/distributions/storage-examples-1.0-SNAPSHOT.zip"),
     handler: "org.http4k.example.StorageFunction",
     role: defaultRole.arn,
     runtime: "java11",
+    timeout: 15
+});
+
+const loggingPermission = new aws.lambda.Permission("http4k-storage-example-lambda-logging-permission", {
+    action: "lambda:InvokeFunction",
+    "function": storageFunction.name,
+    principal: "logs.eu-west-1.amazonaws.com",
+    sourceArn: pulumi.interpolate`${logGroupApi.arn}:*`,
 });
 
 const apiGatewayPermission = new aws.lambda.Permission("http4k-storage-example-lambda-gateway-permission", {
@@ -40,8 +52,12 @@ const storageApi = new aws.apigatewayv2.Api("http4k-storage-example-api", {
 const storageApiDefaultStage = new aws.apigatewayv2.Stage("default", {
     apiId: storageApi.id,
     autoDeploy: true,
-    name: "$default"
-});
+    name: "$default",
+    accessLogSettings: {
+        destinationArn: logGroupApi.arn,
+        format: `{"requestId": "$context.requestId", "requestTime": "$context.requestTime", "httpMethod": "$context.httpMethod", "httpPath": "$context.path", "status": "$context.status", "integrationError": "$context.integrationErrorMessage"}`
+    }
+})
 
 const storageApiLambdaIntegration = new aws.apigatewayv2.Integration("http4k-storage-example-api-lambda-integration", {
     apiId: storageApi.id,
